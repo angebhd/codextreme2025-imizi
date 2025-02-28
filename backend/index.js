b9b006b1f1461cfe956b01bd7c29691a24f2c70d
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2025-02-28 11:53:57 +0200

    backend - Adding location tracking feature

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const http = require("http");
 const bodyParser = require('body-parser');
-
 
 const passport = require('passport');
 
@@ -15,8 +15,10 @@ const userRoutes = require('./src/routes/userRoutes');
 const familyRoutes = require('./src/routes/familyRoutes');
 const taskRoutes = require('./src/routes/taskRoutes');
 
-
+/* Create express and http server */
 const app = express();
+const server = http.createServer(app);
+
 /* Use middlewars */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,6 +33,11 @@ app.use('/api/users', userRoutes);
 app.use('/api/family', familyRoutes);
 app.use('/api/task', taskRoutes);
 
+/* Import and initialize websocket */
 
-app.listen(process.env.PORT, ()=> console.log(`Listening on ${process.env.PORT}`));
+require("./src/services/locationServices")(server); // Pass `server` to WebSocket module
+
+
+
+server.listen(process.env.PORT, ()=> console.log(`Listening on ${process.env.PORT}`));
 console.log(new Date())
